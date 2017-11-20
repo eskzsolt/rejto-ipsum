1c4eba4885fdb23f9ae357b3eb012815f0b6c167
Zsolt Eskulits <eskzsolt@users.noreply.github.com>
2017-11-20 21:03:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const local_jokes = text.slice();
 // Internet Explorer 6-11
 const isIE = /*@cc_on!@*/false || !!document.documentMode;
 
+const containedBy = function(ar, q) {
+  return ar.filter(function(b) {
+    return b === q;
+  }).length > 0 ? true : false;
+}
+
 function newJokes() {
 
   let selected_jokes = [];
@@ -14,13 +20,12 @@ function newJokes() {
       while (selected_jokes.includes(pick)) {
         pick = local_jokes[Math.floor(Math.random() * local_jokes.length)];
       }
-      selected_jokes.push(i % 10 === 0 && i !== 0 ? '<br><br>' + pick : pick);
     } else {
-      while (selected_jokes.includes(pick)) {
+      while (containedBy(selected_jokes, pick)) {
         pick = local_jokes[Math.floor(Math.random() * local_jokes.length)];
       }
-      selected_jokes.push(i % 10 === 0 && i !== 0 ? '<br><br>' + pick : pick);  
     }
+    selected_jokes.push(i % 10 === 0 && i !== 0 ? '<br><br>' + pick : pick); 
   }
 
   return selected_jokes.join(' ');
