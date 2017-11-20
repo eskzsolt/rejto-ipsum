122d74396ce96efd58bf6c2a59f5f1cd3fee2d97
Zsolt Eskulits <eskzsolt@users.noreply.github.com>
2017-11-20 20:59:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const local_jokes = text.slice();
 
+// Internet Explorer 6-11
+const isIE = /*@cc_on!@*/false || !!document.documentMode;
+
 function newJokes() {
 
-    let selected_jokes = [];
-    let current_selection = [];
-
-    for (let i = 0; i < 20; i+=1) {
-        let pick = local_jokes[Math.floor(Math.random() * local_jokes.length)];
-        while (selected_jokes.includes(pick)) {
-            pick = local_jokes[Math.floor(Math.random() * local_jokes.length)];
-        }
-        selected_jokes.push(i % 10 === 0 && i !== 0 ? '<br><br>' + pick : pick);
+  let selected_jokes = [];
+  let current_selection = [];
+    
+  for (let i = 0; i < 20; i+=1) {
+    let pick = local_jokes[Math.floor(Math.random() * local_jokes.length)];
+    if (!isIE) {
+      while (selected_jokes.includes(pick)) {
+        pick = local_jokes[Math.floor(Math.random() * local_jokes.length)];
+      }
+      selected_jokes.push(i % 10 === 0 && i !== 0 ? '<br><br>' + pick : pick);
+    } else {
+      while (selected_jokes.includes(pick)) {
+        pick = local_jokes[Math.floor(Math.random() * local_jokes.length)];
+      }
+      selected_jokes.push(i % 10 === 0 && i !== 0 ? '<br><br>' + pick : pick);  
     }
+  }
 
-    return selected_jokes.join(' ');
+  return selected_jokes.join(' ');
 
 }
 
@@ -26,19 +36,19 @@ document.getElementById("main").innerHTML = newJokes();
 let myEl = document.getElementById('refresh');
 
 myEl.addEventListener('click', function() {
-    document.getElementById("main").innerHTML = newJokes();
+  document.getElementById("main").innerHTML = newJokes();
 }, false);
 
 document.getElementById("add").addEventListener('click', function() {
-    document.getElementById("main").innerHTML = addJokes();
+  document.getElementById("main").innerHTML = addJokes();
 }, false);
 
 
 $(document).ready(function() {
-    var clipboard = new Clipboard('#copy');
-    clipboard.on('success', function(e) {
-        e.clearSelection();
-        $("#copy, #main").fadeOut('slow', function() {
-        }).fadeIn();
-    });
+  var clipboard = new Clipboard('#copy');
+  clipboard.on('success', function(e) {
+    e.clearSelection();
+    $("#copy, #main").fadeOut('slow', function() {
+      }).fadeIn();
+  });
 });
