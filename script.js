93f1910175835eef6c1d7cdfe33c9ca7d25f4c29
Zsolt Eskulits <eskzsolt@gmail.com>
2018-07-18 17:42:29 +0200

    Wrapped script in function

--- script.js
+++ script.js
@@ -1,48 +1,52 @@
-const local_jokes = text.slice();
+(function() {
 
-const containedBy = function(ar, q) {
-  return ar.filter(function(b) {return b === q}).length > 0 ? true : false;
-}
+  const local_jokes = text.slice();
 
-function newJokes() {
-
-  let selected_jokes = [];
-  let current_selection = [];
-    
-  for (let i = 0; i < 20; i+=1) {
-    let pick = local_jokes[Math.floor(Math.random() * local_jokes.length)];
-    while (containedBy(selected_jokes, pick)) {
-      pick = local_jokes[Math.floor(Math.random() * local_jokes.length)];
-    }
-    selected_jokes.push(i % 10 === 0 && i !== 0 ? '<br><br>' + pick : pick); 
+  const containedBy = function(ar, q) {
+    return ar.filter(function(b) {return b === q}).length > 0 ? true : false;
   }
 
-  return selected_jokes.join(' ');
+  function newJokes() {
 
-}
+    let selected_jokes = [];
+    let current_selection = [];
 
-function addJokes() {
-  return document.getElementById("main").innerHTML += '<br><br>' + newJokes();
-}
+    for (let i = 0; i < 20; i+=1) {
+      let pick = local_jokes[Math.floor(Math.random() * local_jokes.length)];
+      while (containedBy(selected_jokes, pick)) {
+        pick = local_jokes[Math.floor(Math.random() * local_jokes.length)];
+      }
+      selected_jokes.push(i % 10 === 0 && i !== 0 ? '<br><br>' + pick : pick);
+    }
 
-document.getElementById("main").innerHTML = newJokes();
+    return selected_jokes.join(' ');
 
-let myEl = document.getElementById('refresh');
+  }
 
-myEl.addEventListener('click', function() {
+  function addJokes() {
+    return document.getElementById("main").innerHTML += '<br><br>' + newJokes();
+  }
+
   document.getElementById("main").innerHTML = newJokes();
-}, false);
 
-document.getElementById("add").addEventListener('click', function() {
-  document.getElementById("main").innerHTML = addJokes();
-}, false);
+  let myEl = document.getElementById('refresh');
+
+  myEl.addEventListener('click', function() {
+    document.getElementById("main").innerHTML = newJokes();
+  }, false);
+
+  document.getElementById("add").addEventListener('click', function() {
+    document.getElementById("main").innerHTML = addJokes();
+  }, false);
 
 
-$(document).ready(function() {
-  var clipboard = new Clipboard('#copy');
-  clipboard.on('success', function(e) {
-    e.clearSelection();
-    $("#copy, #main").fadeOut('slow', function() {
-      }).fadeIn();
+  $(document).ready(function() {
+    var clipboard = new Clipboard('#copy');
+    clipboard.on('success', function(e) {
+      e.clearSelection();
+      $("#copy, #main").fadeOut('slow', function() {
+        }).fadeIn();
+    });
   });
-});
+
+})();
